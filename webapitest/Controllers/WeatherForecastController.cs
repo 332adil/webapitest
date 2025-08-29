28b0069feb0d05ea3e7004a94cb8fb5133ad7d9c
Adil Mathakiya <mathakiyaadil332@gmail.com>
2025-08-29 14:36:47 +0530

    remove error

--- webapitest/Controllers/WeatherForecastController.cs
+++ webapitest/Controllers/WeatherForecastController.cs
@@ -11,7 +11,7 @@ namespace webapitest.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Cold", "Spicy"
         };
 
-        //private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
